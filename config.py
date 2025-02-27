8b3b3103ff55c5e1e30413dfbb5ac8aea4e1e6dd
ap75 <p.aleksieiev@rl.kyiv.ua>
2025-02-27 09:17:15 +0200

    Add config

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'shop.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
+    MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 MB
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
