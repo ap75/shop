ff5bd6442a5ea1c3ad4730d219c72c8b78043e63
ap75 <pavel.alexeyev@gmail.com>
2025-03-05 21:50:13 +0200

    Fix models

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from app import db
 
 
-class Categories(db.Model):
+class Category(db.Model):
+    __tablename__ = 'categories'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     products = db.relationship('Product', backref='category', lazy=True, cascade="all, delete")
@@ -13,8 +13,14 @@ class Categories(db.Model):
 
 
 class Product(db.Model):
+    __tablename__ = 'products'
+
     id = db.Column(db.Integer, primary_key=True)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
     name = db.Column(db.String(200), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     picture = db.Column(db.String(255))
+    description = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return f"<Product {self.name}>"
