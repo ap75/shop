d1f4ac57df730474aa90f0c0dc2836cab114910a
ap75 <pavel.alexeyev@gmail.com>
2025-03-05 22:02:55 +0200

    Add SECRET_KEY for flask_admin

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'shop.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Вимикаємо відстеження змін для покращення продуктивності
 
+    # Секретний ключ для безпеки сесій
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "Таємний_Ключ_Захисту_Фласк_2025!"
+
     # Налаштування для завантаження файлів
     UPLOAD_FOLDER = os.path.join(BASE_DIR, 'static', 'uploads')
     MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100 MB
