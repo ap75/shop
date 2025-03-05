c949c2f6b10303cdcd61cef7cd58d979e4cb74a8
ap75 <pavel.alexeyev@gmail.com>
2025-03-05 22:47:01 +0200

    Login page

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template, flash
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_login import login_user, logout_user, current_user
 
 from config import Config
 from extensions import db, login_manager
@@ -20,21 +21,47 @@ login_manager.init_app(app)
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+# Захист адмін-панелі
+class AdminModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("login", next=request.url))
+
+# Додаємо Flask-Admin
+admin = Admin(app, name="Адмінка магазину", template_mode="bootstrap4")
+admin.add_view(AdminModelView(Category, db.session))
+admin.add_view(AdminModelView(Product, db.session))
+admin.add_view(AdminModelView(User, db.session))
+
+# Сторінка входу
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:  # Тут треба додати хешування
+            login_user(user)
+            return redirect(url_for("admin.index"))
+
+        flash("Неправильний логін або пароль")
+
+    return render_template("login.html")
+
+
+# Сторінка виходу
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
 # Створення таблиць, якщо їх ще немає
 with app.app_context():
     db.create_all()  # Створення таблиць
-
-with app.app_context():
-    from sqlalchemy import inspect
-    inspector = inspect(db.engine)
-    print("Таблиці в базі даних:", inspector.get_table_names())  # Перевіряємо таблиці
-
-# Ініціалізація Flask-Admin
-admin = Admin(app, name="Адмінка магазину", template_mode="bootstrap4")
-
-# Додаємо моделі до адмін-панелі
-admin.add_view(ModelView(Category, db.session))
-admin.add_view(ModelView(Product, db.session))
 
 
 if __name__ == "__main__":
